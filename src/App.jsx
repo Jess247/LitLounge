60c85ba81ca6749e527c3998b02055ab11ddddee
Jess247 <jesss8692@hotmail.com>
2024-05-30 14:50:10 +0200

    Add react router and create rountes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
 import { useState } from 'react'
-import Navigation from './components/Navigation'
-import Hero from './components/Hero'
-import Footer from './components/Footer'
+import Home from "./pages/Home"
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
+import ErrorPage from './pages/ErrorPage'
+
+const router = createBrowserRouter([
+  {
+  path:"/",
+  element: <Home/>,
+  errorElement:<ErrorPage/>
+  },
+  {
+    path:"books",
+    element: <h1>Books</h1>
+  },
+  {
+    path:"login",
+    element: <h1>Login</h1>
+  }
+])
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Navigation/>
-      <Hero/>
-      <Footer/>
+    <RouterProvider router={router} />
     </>
   )
 }
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,6 +1,30 @@
+import {Link} from "react-router-dom"
+import { FaBookOpen } from "react-icons/fa6"
+
 function Navigation() {
     return (
-        <h1>Navigation</h1>
+        <header>
+            <div>
+                <FaBookOpen/> Lit<span>Longe</span>
+            </div>
+            <ul>
+            <Link
+                to="/"
+                >
+                <li>Home</li>
+            </Link>
+            <Link
+                to="books"
+                >
+                <li>my books</li>
+            </Link>
+            <Link
+                to="login"
+                >
+                <li>Login</li>
+            </Link>
+        </ul>
+        </header>
     )
 }
 
