fa6ed44c269a35c26d1994a1185166bb427d8961
Jess247 <jesss8692@hotmail.com>
2024-06-13 12:46:40 +0200

    Add Hero section with searchbar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,29 @@
+import hero from '../assets/hero.jpg'
+
 function Hero() {
     return (
-        <h1 className="text-blue-700">Hero</h1>
+        <section 
+            className={` h-[60vh] overflow-hidden`}
+            style={{backgroundImage: "url(`${hero}`)"}}>
+            <img src={hero} 
+                 alt="Two open books in front of a blurry bookshelf."
+                 className='relative z-0 object-fit'
+            />
+            <div className='bg-slate-50 h-10 z-10 absolute top-[50vh] left-1/2 -translate-x-1/2 w-2/3 pl-2 rounded-3xl gap-2 flex max-w-4xl'>
+                <label 
+                    htmlFor="search"
+                    className='opacity-0 absolute'>
+                    Search books
+                </label>
+                <input
+                    type="text" 
+                    placeholder='Author, Title, Keyword...'
+                    className='font-light text-sm p-1 bg-transparent w-full'/>
+                <button className='bg-slate-200 h-full py-2 px-4 rounded-r-3xl font-bold hover:bg-slate-300'>
+                    Search
+                </button>
+            </div>
+        </section>
     )
 }
 
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,29 +1,37 @@
 import {Link} from "react-router-dom"
 import { FaBookOpen } from "react-icons/fa6"
+import { RxHamburgerMenu } from "react-icons/rx"
 
 function Navigation() {
+    const isHidden = true
+    const isMobile = true
+    const hideNav = "absolute -top-40 -left-60"
+
     return (
-        <header >
-            <div>
+        <header className={`flex justify-between p-8`} >
+            <div className="flex items-center gap-1">
                 <FaBookOpen/> Lit<span>Longe</span>
             </div>
-            <ul className="flex">
-            <Link
-                to="/"
-                >
-                <li>Home</li>
-            </Link>
-            <Link
-                to="books"
-                >
-                <li>my books</li>
-            </Link>
-            <Link
-                to="login"
-                >
-                <li>Login</li>
-            </Link>
-        </ul>
+            <div>
+                <button><RxHamburgerMenu size={25}/></button>
+                <ul className={`flex gap-4 ${isHidden ? hideNav : null}  ${isMobile ? "flex-col mt-4  bg-red-50 w-full" : null}`}>
+                    <Link
+                        to="/"
+                        >
+                        <li>Home</li>
+                    </Link>
+                    <Link
+                        to="books"
+                        >
+                        <li>my books</li>
+                    </Link>
+                    <Link
+                        to="login"
+                        >
+                        <li>Login</li>
+                    </Link>
+                </ul>
+            </div>
         </header>
     )
 }
