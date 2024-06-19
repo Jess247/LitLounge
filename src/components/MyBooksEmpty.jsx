ab33428bbd995c9e1012e59cc72143e5691bdc82
Jess247 <jesss8692@hotmail.com>
2024-06-19 13:43:33 +0200

    Fix emty book library layout

--- src/components/MyBooksEmpty.jsx
+++ src/components/MyBooksEmpty.jsx
@@ -2,7 +2,7 @@ import { MdOutlineMenuBook } from "react-icons/md"
 
 export default function MyBooksEmpty() {
     return(
-        <section className="flex flex-col items-center gap-2 mx-autos h-80 justify-center text-gray-500">
+        <section className="flex flex-col items-center gap-2 mt-[15%] text-gray-500">
             <h2 className="border-t-2 rounded-xl pt-4 px-4">Your Library is empty</h2>
             <MdOutlineMenuBook size={50}/>
             <p className="border-b-2 rounded-xl pb-4 px-4">Add books more book</p>
